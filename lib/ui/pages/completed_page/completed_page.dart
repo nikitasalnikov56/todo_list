be9a3e8e87bf1eb491be717a16f695693643b680
nik <nik@ya.ru>
2024-06-26 13:50:57 +0500

    completed page

--- lib/ui/pages/completed_page/completed_page.dart
+++ lib/ui/pages/completed_page/completed_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_list/domain/database/hive_box.dart';
 // import 'package:provider/provider.dart';
 // import 'package:todo_list/domain/provider/todo_provider.dart';
 import 'package:todo_list/ui/components/card_widget.dart';
@@ -14,16 +16,22 @@ class CompletedPage extends StatelessWidget {
     return Container(
       color: AppColors.background,
       padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 22),
-      child: ListView.separated(
-        itemBuilder: (context, i) {
-          return CardWidget(
-            title: 'TODO TITLE',
-            subTitle: 'TODO SUB TITLE',
-            index: i,
+      child: ValueListenableBuilder(
+        valueListenable: HiveBox.notes.listenable(),
+        builder: (context, notes, _) {
+          final allNotes = notes.values.toList();
+          return ListView.separated(
+            itemBuilder: (context, i) {
+              return allNotes[i].isChecked ? CardWidget(
+                title: '${allNotes[i].title}',
+                subTitle: '${allNotes[i].text}',
+                index: i,
+              ): const SizedBox();
+            },
+            separatorBuilder: (context, i) => const SizedBox(height: 21),
+            itemCount: allNotes.length,
           );
-        },
-        separatorBuilder: (context, i) => const SizedBox(height: 21),
-        itemCount: 1,
+        }
       ),
     );
   }
